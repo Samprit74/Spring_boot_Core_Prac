9b5c2d3142f9bd581c83c6f76f33334a219eb4ee
Samprit Roy <sampritroy99@gmail.com>
2026-08-29 23:53:02 +0530

    added new line

--- src/main/java/org/samp/Main.java
+++ src/main/java/org/samp/Main.java
@@ -17,9 +17,10 @@ public class Main {
     System.out.println(i+Integer.MAX_VALUE);
 
     System.out.println(i+Integer.MIN_VALUE+Double.MAX_VALUE);
+        System.out.println(i+Integer.MIN_VALUE+Double.MAX_VALUE);
 
 
 
-        }
+    }
 
 }
